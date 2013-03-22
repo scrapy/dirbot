0edb745020e6f83ea6a21e6dbea9b0446623db49
Pablo Hoffman <pablo@pablohoffman.com>
2013-03-22 15:02:25 -0300

    remove custom __str__ from Website item

--- dirbot/items.py
+++ dirbot/items.py
@@ -6,6 +6,3 @@ class Website(Item):
     name = Field()
     description = Field()
     url = Field()
-
-    def __str__(self):
-        return "Website: name=%s url=%s" % (self.get('name'), self.get('url'))
